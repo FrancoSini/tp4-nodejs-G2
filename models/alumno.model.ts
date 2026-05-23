6ed2384f6f7ba7fe0f8690c6ca8cf425286ca947
mateeoo2003@icloud.com <2ndxsmmnm8@privaterelay.appleid.com>
2026-05-23 14:41:52 -0300

    Correcciones para que funcionen los POST,PUT,DELETE en POSTMAN

--- models/alumno.model.ts
+++ models/alumno.model.ts
@@ -1,65 +1,47 @@
 import { PersonaModel } from './persona.model'
 
-// La clase AlumnoModel hereda de PersonaModel
 export class AlumnoModel extends PersonaModel {
-  // Atributos privados propios del alumno
   private legajo: number
   private fechaAlta: string
   private modificacion: string
   private isActive: boolean
 
-  // Constructor de la clase
   constructor(
     nombre: string,
     apellido: string,
     email: string,
     legajo: number,
-
-    // Si no se pasa fecha, se usa la fecha actual
     fechaAlta: string = new Date().toISOString().split('T')[0],
     modificacion: string = new Date().toISOString().split('T')[0],
-
-    // Por defecto el alumno se crea activo
     isActive: boolean = true
   ) {
-    // Llamo al constructor de PersonaModel
     super(nombre, apellido, email)
-
-    // Inicializo los atributos propios
     this.legajo = legajo
     this.fechaAlta = fechaAlta
     this.modificacion = modificacion
     this.isActive = isActive
   }
 
-  // Getter del legajo
+  // Getters y Setters
   public getLegajo(): number {
     return this.legajo
   }
 
-  // Getter del estado activo
   public getIsActive(): boolean {
     return this.isActive
   }
-
-  // Setter para cambiar el estado
   public setIsActive(status: boolean): void {
     this.isActive = status
   }
 
-  // Getter de la fecha de modificación
   public getModificacion(): string {
     return this.modificacion
   }
-
-  // Setter de la fecha de modificación
   public setModificacion(fecha: string): void {
     this.modificacion = fecha
   }
 
-  // Polimorfismo:
-  // Sobrescribo el método de la clase padre
-  // para devolver todos los atributos del alumno
+  // Polimorfismo
   public override getAllAttributes(): {
     legajo: number
     nombre: string
@@ -69,16 +51,11 @@ export class AlumnoModel extends PersonaModel {
     modificacion: string
     isActive: boolean
   } {
-    // Retorno un objeto con todos los datos
     return {
       legajo: this.legajo,
-
-      // Estos atributos vienen de PersonaModel
-      // y se pueden usar porque son protected
-      nombre: this.nombre,
+      nombre: this.nombre, // Disponibles porque son 'protected' en PersonaModel
       apellido: this.apellido,
       email: this.email,
-
       fechaAlta: this.fechaAlta,
       modificacion: this.modificacion,
       isActive: this.isActive

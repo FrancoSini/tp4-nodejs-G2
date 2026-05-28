62555612209d0dce98b7f3a2019f5f1907b08bf8
ricardo herbas <ricardosebaherbas@gmail.com>
2026-05-28 21:16:59 +0000

    cambios en materias.routes , materia.controller y server.js

--- core/server.js
+++ core/server.js
@@ -17,10 +17,8 @@ class Server {
 
   rutas() {
     this.app.use('/alumnos', require('../routes/alumno.routes'))
-    /*
-    this.app.use('/materias', require('../routes/extra/materia.routes'))
-    this.app.use('/notas', require('../routes/extra/nota.routes'))
-    */
+    this.app.use('/materias', require('../routes/extras/materia.routes'))
+    //this.app.use('/notas', require('../routes/extra/nota.routes'))
     this.app.use('/profesores', require('../routes/extras/profesor.routes'))
     // manejo de errores
     this.app.use((req, res, next) => {
--- routes/extras/materia.routes.js
+++ routes/extras/materia.routes.js
@@ -1,12 +1,20 @@
 const { Router } = require('express')
 const {
   getMateriaAll,
-  getMateriaById
-} = require('../controllers/extras/materia.controller')
+  getMateriaById,
+  postNewMateria,
+  putMateriaById,
+  deleteMateriaById
+} = require('../../controllers/materia.controller')
+
+const { validateInputMateria } = require('../../middleware/materia-validator.middleware')
 
 const rutas = Router()
 
 rutas.get('/', getMateriaAll)
 rutas.get('/:id', getMateriaById)
+rutas.post('/', validateInputMateria, postNewMateria)
+rutas.put('/:id', validateInputMateria, putMateriaById)
+rutas.delete('/:id', deleteMateriaById)
 
 module.exports = rutas
